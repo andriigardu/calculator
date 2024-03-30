0246c986ea551baeefe531edc8713b5b2396b281
andriigardu <122264568+andriigardu@users.noreply.github.com>
2024-03-30 20:18:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,101 @@
+      function addToDisplay(val) {
+        document.getElementById('display').value += val;
+      }
 
+      function backspace() {
+        let expression = document.getElementById('display').value;
+        document.getElementById('display').value = expression.slice(0, -1);
+      }
+
+      function clearDisplay() {
+        document.getElementById('display').value = '';
+      }
+
+      function calculate() {
+        let expression = document.getElementById('display').value;
+        let tokens = tokenize(expression);
+        let result = evaluate(tokens);
+        document.getElementById('display').value = result;
+      }
+
+      function tokenize(expression) {
+        let tokens = [];
+        let currentToken = '';
+
+        for (let char of expression) {
+          if ('0123456789.'.includes(char)) {
+            currentToken += char;
+          } else {
+            if (currentToken) {
+              tokens.push(currentToken);
+              currentToken = '';
+            }
+
+            if ('+-*/'.includes(char)) {
+              tokens.push(char);
+            }
+          }
+        }
+
+        if (currentToken) {
+          tokens.push(currentToken);
+        }
+
+        return tokens;
+      }
+
+      function evaluate(tokens) {
+        let operatorPrecedence = {
+          '+': 1,
+          '-': 1,
+          '*': 2,
+          '/': 2,
+        };
+        let operandStack = [];
+        let operatorStack = [];
+
+        for (let token of tokens) {
+          if (!isNaN(token)) {
+            operandStack.push(Number(token));
+          } else {
+            while (operatorStack.length > 0 && operatorPrecedence[operatorStack[operatorStack.length - 1]] >= operatorPrecedence[token]) {
+              let operator = operatorStack.pop();
+              let operand2 = operandStack.pop();
+              let operand1 = operandStack.pop();
+              let result = evaluateOperation(operator, operand1, operand2);
+              operandStack.push(result);
+            }
+
+            operatorStack.push(token);
+          }
+        }
+
+        while (operatorStack.length > 0) {
+          let operator = operatorStack.pop();
+          let operand2 = operandStack.pop();
+          let operand1 = operandStack.pop();
+          let result = evaluateOperation(operator, operand1, operand2);
+          operandStack.push(result);
+        }
+
+        return operandStack[0];
+      }
+
+      function evaluateOperation(operator, operand1, operand2) {
+        switch (operator) {
+          case '+':
+            return operand1 + operand2;
+          case '-':
+            return operand1 - operand2;
+          case '*':
+            return operand1 * operand2;
+          case '/':
+            return operand1 / operand2;
+        }
+      }
+
+      document.addEventListener('keydown', function (event) {
+  if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+    calculate();
+  }
+});
